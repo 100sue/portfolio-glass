593aed1b51b53aef2efb6c9bbeb3b22e10684701
appert <scoreur@gmail.com>
2022-11-15 21:32:19 +0100

    fonctionnement js, responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,56 @@
+window.addEventListener("load", () => {
+    document.querySelector(".main").classList.remove("hidden");
+    document.querySelector(".home-section").classList.add("active");
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout(() => {
+        document.querySelector(".page-loader").style.display = "none"; 
+    },600);
+
+})
+
+
+/*---------- Toggle Navbar ---------*/
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+})
+function hideSection() {
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar() {
+    document.querySelector(".header").classList.toggle("active");
+}
+
+/*---------- Active section ---------*/
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if (e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else {
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        },500);
+    }
+});
+
+
+
+
+
 /*---------- About tabs ---------*/
 const tabscontainer = document.querySelector(".about-tabs"),
 aboutSection = document.querySelector(".about-section");
@@ -11,6 +64,7 @@ tabscontainer.addEventListener("click", (e)=> {
         aboutSection.querySelector(target).classList.add("active");
     }
 })
+
 
 /*---------- Porfolio Popup ---------*/
 
@@ -27,6 +81,7 @@ function togglePortfolioPopup() {
     document.querySelector(".main").classList.toggle("fade-out");
 }
 document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
 
 /*--- Hide popup when clicking outside ---*/
 
@@ -48,3 +103,5 @@ function portfolioItemDetails(portfolioItem) {
     portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 
 }
+
+
